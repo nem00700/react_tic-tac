b67c46046732c76b7db09f92d7fc5b0b4fe9d775
Timur Gafiulin <nem00700@gmail.com>
2017-10-17 21:19:07 +0300

    Add react-touter-dom

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Route path="/" component={App} >
+        </Route>
+    </Router>, 
+    document.getElementById('root')
+);
