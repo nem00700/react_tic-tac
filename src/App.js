d0393d17f99149d15b89501d41dc8bb1864de8a0
Timur Gafiulin <nem00700@gmail.com>
2017-10-19 19:13:11 +0300

    add react-router in App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,15 @@ import {
 
 class App extends Component {
   render() {
-    return <div>
-      <GameField />
-    </div>;
+    return <Router>
+      <div>
+        <button><Link to="/1vs1">1 vs 1</Link></button>
+        <button><Link to="/vscomputer">1 vs Computer</Link></button>
+        
+        <Route path="/1vs1" component={GameField}/>
+        <Route path="/vscomputer" component={GameField}/>
+      </div>
+    </Router>;
   }
 }
 
