0ca1388da7388da946a5ea20a45adafd622166e9
Timur Gafiulin <nem00700@gmail.com>
2017-10-19 19:01:08 +0300

    move react-router in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import GameField from './components/GameField';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -2,16 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {
-    BrowserRouter as Router,
-    Route,
-    Link
-  } from 'react-router-dom'
 
 ReactDOM.render(
-    <Router>
-        <Route path="/" component={App} >
-        </Route>
-    </Router>, 
+    <App />, 
     document.getElementById('root')
 );
