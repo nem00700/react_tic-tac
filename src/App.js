56f8301c520b0c687da4601ac6181a591629110f
Timur Gafiulin <nem00700@gmail.com>
2017-10-24 20:05:43 +0300

    fix react-router (buttons should not be reflected during the game)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import VsComputer from './components/VsComputer';
-import VsPerson from './components/VsPerson';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
-
+import {Link } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
-    return <Router>
-      <div>
-        
+    return <div>
         <button><Link to="/1vs1">1 vs 1</Link></button>
-        <button><Link to="/vscomputer">1 vs Computer</Link></button>
-
-        <Route path="/1vs1" component={VsPerson}/>
-        <Route path="/vscomputer" component={VsComputer}/>
-      </div>
-    </Router>;
+        <button><Link to="/vscomputer">1 vs Computer</Link></button> 
+      </div>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import VsPerson from "./components/VsPerson";
+import VsComputer from "./components/VsComputer";
+
 
 ReactDOM.render(
-    <App />, 
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/1vs1" component={VsPerson} />
+            <Route path="/vscomputer" component={VsComputer} />
+        </Switch>
+    </Router>, 
     document.getElementById('root')
 );
