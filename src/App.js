cccc3ff63d45d9669cc7a4f1b03ee94dc9e304a5
Timur Gafiulin <nem00700@gmail.com>
2017-11-05 15:30:06 +0300

    add style for main menu

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import {Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return <div>
-        <button><Link to="/1vs1">1 vs 1</Link></button>
-        <button><Link to="/vscomputer">1 vs Computer</Link></button> 
+    return <div className='nav-block'>
+        <Link to="/1vs1"><nav>1 vs 1</nav></Link>
+        <Link to="/vscomputer"><nav>1 vs Computer</nav></Link>
       </div>;
   }
 }
