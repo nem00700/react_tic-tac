42662211701c734148892fbf43d4555a4d7c642b
Timur Gafiulin <nem00700@gmail.com>
2017-11-19 21:03:22 +0300

    add reducer for game

--- src/AC/index.js
+++ src/AC/index.js
@@ -3,6 +3,6 @@ import { CHANGE_GAMEFIELD_SIZE } from "../constants";
 export function changeGamefieldSize (size){
     return{
         type: CHANGE_GAMEFIELD_SIZE,
-        gamfieldSize: size,
+        gameFieldSize: size,
     }
 }
