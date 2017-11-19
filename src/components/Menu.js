05de13394a1bb97a37b30cc9c1ba150b26263743
Timur Gafiulin <nem00700@gmail.com>
2017-11-19 21:11:01 +0300

    add state for size of game field

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,12 +3,22 @@ import {Link } from 'react-router-dom';
 
 
 class Menu extends Component {
-  render() {
-    return <div className='nav-block'>
-        <Link to="/1vs1"><nav>1 vs 1</nav></Link>
-        <Link to="/vscomputer"><nav>1 vs Computer</nav></Link>
-      </div>;
-  }
+    state = {
+        size: 3,
+    }
+
+    handleChange(event){
+        this.setState({ size: event.target.value });
+    }
+
+
+    render() {
+        return <div className='nav-block'>
+            <input val={this.state.size} onChange={this.handleChange.bind(this)} />
+            <Link to="/1vs1"><nav>1 vs 1</nav></Link>
+            <Link to="/vscomputer"><nav>1 vs Computer</nav></Link>
+        </div>;
+    }
 }
 
 export default Menu;
