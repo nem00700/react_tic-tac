1f8b68d7bb82c04983d791573602faa89674a8d2
Timur Gafiulin <nem00700@gmail.com>
2017-11-20 20:33:34 +0300

    add redux-logger

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger'
 import reducer from './reducers';
 
-let store = createStore(reducer);
+let store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
     <Provider store={store}>
